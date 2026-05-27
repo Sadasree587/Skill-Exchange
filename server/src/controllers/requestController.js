028cb1c3a06b6ae279ba183a8d6e7db47a00be9a
Sadasree <sadasadasree@gmail.com>
2026-05-27 21:58:50 +0530

    Completed request deletion and authorization APIs

--- server/src/controllers/requestController.js
+++ server/src/controllers/requestController.js
@@ -152,9 +152,54 @@ const updateRequestStatus = async (req, res) => {
 
 };
 
+// DELETE REQUEST
+const deleteRequest = async (req, res) => {
+
+  try {
+
+    // Find request
+    const request = await SwapRequest.findById(
+      req.params.id
+    );
+
+    if (!request) {
+      return res.status(404).json({
+        message: "Request not found",
+      });
+    }
+
+    // Only sender can delete
+    if (
+      request.sender.toString() !==
+      req.user._id.toString()
+    ) {
+      return res.status(401).json({
+        message:
+          "Not authorized to delete this request",
+      });
+    }
+
+    // Delete request
+    await request.deleteOne();
+
+    res.status(200).json({
+      message: "Request deleted successfully",
+    });
+
+  } catch (error) {
+
+    res.status(500).json({
+      message: error.message,
+    });
+
+  }
+
+};
+
 module.exports = {
   sendSwapRequest,
   getSentRequests,
   getReceivedRequests,
   updateRequestStatus,
+  deleteRequest,
 };
--- server/src/routes/requestRoutes.js
+++ server/src/routes/requestRoutes.js
@@ -5,6 +5,7 @@ const {
   getSentRequests,
   getReceivedRequests,
   updateRequestStatus,
+  deleteRequest,
 } = require("../controllers/requestController");
 
 const protect = require("../middleware/authMiddleware");
@@ -18,5 +19,7 @@ router.get("/sent", protect, getSentRequests);
 router.get("/received", protect, getReceivedRequests);
 
 router.put("/status/:id", protect, updateRequestStatus);
+
+router.delete("/:id", protect, deleteRequest);
 
 module.exports = router;
